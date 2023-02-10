c90aec8ded5372ca389bf0e084fc61c3ef6e8592
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-11 00:14:05 +0800

    Build add book button function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const addBtn = document.querySelector(".add-btn");
+
 //Store all books here
 let myLibrary = [];
 
@@ -11,3 +13,9 @@ function book(title, author, pages, read) {
 
 //Individual book
 const book1 = new book("kong", "sinoi", 244, true);
+
+//Add button
+addBtn.addEventListener("click", () => {
+  myLibrary.push(book1);
+  console.log(myLibrary);
+});
