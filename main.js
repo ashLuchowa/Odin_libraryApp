ac20b5c0b22c6be90e22b514bfc42733179d0f48
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-10 17:57:04 +0800

    Add event listener to push items into library array

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+const libraryContainer = document.querySelector(".library-container");
+const addBtn = document.querySelector(".add-btn");
+
 //All book stored here
 let myLibrary = ["Book1", "Book2"];
 
-function book() {
-  //the constructor
-}
+// function book() {
+//   //the constructor
+// }
 
-function addBookToLibrary() {
-  //input book data here
-}
+// function addBookToLibrary() {
+//   //input book data here
+// }
 
 console.log(myLibrary);
+
+addBtn.addEventListener("click", () => {
+  myLibrary.push("book");
+  console.log(myLibrary);
+});
